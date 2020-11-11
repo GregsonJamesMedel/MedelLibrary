7e9f8589e84a40d2ab0a1710a337f9ad446dfa73
GregsonJamesMedel <GregsonJamesMedel@Gmail.com>
2020-11-11 18:45:51 +0800

    add signup viewmodel

---
+++ ViewModels/SignUpVM.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedelLibrary.ViewModels
+{
+    public class SignUpVM
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email Adress")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(5)]
+        [MaxLength(20)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
+        public string Middlename { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(15)]
+        public string Lastname { get; set; }
+    }
+}
