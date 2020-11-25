cabec19e897e17f04e8539ea3d7cc53a05f1f5d5
GregsonJamesMedel <GregsonJamesMedel@Gmail.com>
2020-11-25 20:16:05 +0800

    Fix SignIn View

--- ViewModels/SignInVM.cs
+++ ViewModels/SignInVM.cs
@@ -5,8 +5,10 @@ namespace MedelLibrary.ViewModels
     public class SignInVM
     {
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
