c929495fbfd5790259bff96c0596f372be8f9fea
GregsonJamesMedel <GregsonJamesMedel@Gmail.com>
2020-12-06 17:44:59 +0800

    impelemt search results and its view

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using MedelLibrary.Services;
+using MedelLibrary.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedelLibrary.Controllers
@@ -14,7 +16,26 @@ namespace MedelLibrary.Controllers
 
         public IActionResult Results(string query)
         {
-            var model = this._asset.GetAssetsByTitle(query);
+            var results = this._asset.GetAssetsByTitle(query).Select(result => new AssetVM(){
+                Id = result.Id,
+                Title = result.Title,
+                Year = result.Year,
+                Cost = result.Cost,
+                ImageUrl = result.ImageUrl,
+                NumberOfCopies = result.NumberOfCopies,
+                Shelf = result.Shelf,
+                Condition = result.Condition,
+                Category = result.Category,
+                ISBN = this._asset.GetISBN(result.Id),
+                AuthorOrDirector = this._asset.GetAuthorOrDirector(result.Id),
+                Type = this._asset.GetType(result.Id)
+            });
+
+            var model = new AssetCatalogVM()
+            {
+                Assets = results
+            };
+
             return View(model);
         }
     }
