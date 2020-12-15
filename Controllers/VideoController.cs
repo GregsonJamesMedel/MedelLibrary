74cf6eeef0caf53b280fa23c46ac7310063be860
GregsonJamesMedel <GregsonJamesMedel@Gmail.com>
2020-12-15 18:08:31 +0800

    refactor videocontroller

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -30,39 +30,39 @@ namespace MedelLibrary.Controllers
         {
             model.Categories = this._category.GetAllCategories();
 
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var category = this._category.GetCategoryById(model.Category);
-
-            var video = new Video()
+            if (ModelState.IsValid)
             {
-                Title = model.Title,
-                Director = model.Director,
-                Category = category,
-                Shelf = model.Shelf,
-                Year = model.Year,
-                NumberOfCopies = model.NumberOfCopies,
-                Cost = model.Cost,
-                Condition = "New",
-                Status = "Check out",
-                ImageUrl = "NoCover.png"
-            };
+                var category = this._category.GetCategoryById(model.Category);
 
-            var result = this._asset.AddLibraryAsset(video);
+                var video = new Video()
+                {
+                    Title = model.Title,
+                    Director = model.Director,
+                    Category = category,
+                    Shelf = model.Shelf,
+                    Year = model.Year,
+                    NumberOfCopies = model.NumberOfCopies,
+                    Cost = model.Cost,
+                    Condition = "New",
+                    Status = "Check out",
+                    ImageUrl = "NoCover.png"
+                };
 
-            if (result)
-                return RedirectToAction("AssetCatalog", "Asset");
+                var result = this._asset.AddLibraryAsset(video);
 
+                if (result)
+                    return RedirectToAction("AssetCatalog", "Asset");
+            }
             return View(model);
-
         }
 
         [HttpGet]
         public IActionResult EditVideo(int id)
         {
-            //NOTE: add error if video didn't exist
             var video = this._asset.GetAsset(id);
+
+            if (video == null)
+                return RedirectToAction("NotFound", "Error");
 
             var model = new EditVideoVM()
             {
@@ -88,29 +88,28 @@ namespace MedelLibrary.Controllers
         {
             model.Categories = this._category.GetAllCategories();
 
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var video = new Video()
+            if (ModelState.IsValid)
             {
-                Id = model.Id,
-                Title = model.Title,
-                Year = model.Year,
-                Cost = model.Cost,
-                ImageUrl = model.ImageUrl,
-                NumberOfCopies = model.NumberOfCopies,
-                Shelf = model.Shelf,
-                Director = model.Director,
-                Condition = model.Condition,
-                Status = model.Status,
-                Category = this._category.GetCategoryById(model.Category)
-            };
+                var video = new Video()
+                {
+                    Id = model.Id,
+                    Title = model.Title,
+                    Year = model.Year,
+                    Cost = model.Cost,
+                    ImageUrl = model.ImageUrl,
+                    NumberOfCopies = model.NumberOfCopies,
+                    Shelf = model.Shelf,
+                    Director = model.Director,
+                    Condition = model.Condition,
+                    Status = model.Status,
+                    Category = this._category.GetCategoryById(model.Category)
+                };
 
-            var result = this._asset.UpdateAsset(video);
+                var result = this._asset.UpdateAsset(video);
 
-            if (result)
-                return RedirectToAction("Details", "Asset", new { id = video.Id });
-
+                if (result)
+                    return RedirectToAction("Details", "Asset", new { id = video.Id });
+            }
             return View(model);
         }
 
